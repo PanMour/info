2011650b915ca6999f024407b796b688ba50a5bd
PanMour <115315282+PanMour@users.noreply.github.com>
2022-11-13 22:46:36 +0200

    Add files via upload

---
+++ my_lua.lua
@@ -0,0 +1,12 @@
+function Image(img)
+    if img.classes:find('base',1) then
+      local f = io.open("project/" .. img.src, 'r')
+      local doc = pandoc.read(f:read('*a'))
+      f:close()
+      local caption = pandoc.utils.stringify(doc.meta.caption) 
+      local name = pandoc.utils.stringify(doc.meta.name)
+      local AM = pandoc.utils.stringify(doc.meta.AM)
+      local content = "> " .. caption .. "  \n>" .. "Ονοματεπωνυμο Φοιτητη:" .. name .. "Aριθμος Mητρωου:" .. AM
+      return pandoc.RawInline('markdown',content)
+    end
+end
